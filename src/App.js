206a1c729d807b5afa1e17aa43e7a9fe165f3dd8
Goncalo <goncalosantos@Goncalos-MacBook-Pro.local>
2022-07-26 21:54:57 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
-import "./App.css";
+import React, { useState } from 'react'
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
 import RegistrationPage from "./Pages/RegistrationPage";
 import LoginPage from "./Pages/LoginPage";
 import HomePage from "./Pages/HomePage";
-import NavBar from "./Components/NavBar";
-import { useState } from "react";
+
+import "./App.css";
 
 function App() {
   const [isAuthLoading, setIsAuthLoading] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <Routes>
-          <Route
-            path="/"
-            element={
-              <NavBar
-                isAuthLoading={isAuthLoading}
-                setIsAuthLoading={setIsAuthLoading}
-              />
-            }
-          >
+          <Route path="/" element={<NavBar isAuthLoading={isAuthLoading} setIsAuthLoading={setIsAuthLoading} />}>
             <Route index element={<HomePage />} />
-            <Route
-              path="/login"
-              element={
-                <LoginPage
-                  isAuthLoading={isAuthLoading}
-                  setIsAuthLoading={setIsAuthLoading}
-                />
-              }
-            />
-            <Route
-              path="/registration"
-              element={
-                <RegistrationPage
-                  isAuthLoading={isAuthLoading}
-                  setIsAuthLoading={setIsAuthLoading}
-                />
-              }
-            />
+            <Route path="/login" element={<LoginPage isAuthLoading={isAuthLoading} setIsAuthLoading={setIsAuthLoading}/>} />
+            <Route path="/registration" element={<RegistrationPage isAuthLoading={isAuthLoading} setIsAuthLoading={setIsAuthLoading} />} />
           </Route>
         </Routes>
       </header>
